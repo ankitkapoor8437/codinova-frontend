beb128dd110b83edad41bcbe248e32579394c5d5
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-31 04:00:45 +0530

    Implimented the UI component for the ExchangeList table.

--- src/components/UI/ExchangeList.jsx
+++ src/components/UI/ExchangeList.jsx
@@ -1,10 +1,212 @@
+import { Container } from 'postcss'
 import React from 'react'
 
+const exChangeList = [
+
+    
+    {
+        "exchange_id": "BINANCE",
+        "website": "https://www.binance.com/",
+        "name": "Binance",
+        "data_quote_start": "2017-12-18T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_orderbook_start": "2017-12-18T21:50:58.3910192Z",
+        "data_orderbook_end": "2023-07-07T00:00:00.0000000Z",
+        "data_trade_start": "2017-07-14T00:00:00.0000000Z",
+        "data_trade_end": "2023-08-29T00:00:00.0000000Z",
+        "data_symbols_count": 2299,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+    },
+    {
+        "exchange_id": "KRAKEN",
+        "website": "https://www.kraken.com/",
+        "name": "Kraken",
+        "data_quote_start": "2014-07-31T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_orderbook_start": "2014-07-31T13:05:46.0000000Z",
+        "data_orderbook_end": "2023-07-06T00:00:00.0000000Z",
+        "data_trade_start": "2013-10-22T00:00:00.0000000Z",
+        "data_trade_end": "2023-08-29T00:00:00.0000000Z",
+        "data_symbols_count": 809,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0
+        ,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+    },
+    {
+        "exchange_id": "COINBASE",
+        "website": "https://pro.coinbase.com/",
+        "name": "Coinbase Pro (GDAX)",
+        "data_quote_start": "2015-05-17T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_orderbook_start": "2015-05-17T00:51:32.6370000Z",
+        "data_orderbook_end": "2023-07-06T00:00:00.0000000Z",
+        "data_trade_start": "2015-01-14T00:00:00.0000000Z",
+        "data_trade_end": "2023-08-29T00:00:00.0000000Z",
+        "data_symbols_count": 672,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0
+        ,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+    },
+    {
+        "exchange_id": "BITSTAMP",
+        "website": "https://www.bitstamp.net/",
+        "name": "Bitstamp Ltd.",
+        "data_quote_start": "2014-02-24T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_orderbook_start": "2014-02-24T17:43:05.0000000Z",
+        "data_orderbook_end": "2023-07-06T00:00:00.0000000Z",
+        "data_trade_start": "2011-09-13T00:00:00.0000000Z",
+        "data_trade_end": "2023-08-29T00:00:00.0000000Z",
+        "data_symbols_count": 205,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0
+        ,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+    },
+    {
+        "exchange_id": "GEMINI",
+        "website": "https://gemini.com/",
+        "name": "Gemini",
+        "data_quote_start": "2017-03-18T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_orderbook_start": "2017-03-18T22:53:42.0948480Z",
+        "data_orderbook_end": "2023-07-06T00:00:00.0000000Z",
+        "data_trade_start": "2015-10-08T00:00:00.0000000Z",
+        "data_trade_end": "2023-08-29T00:00:00.0000000Z",
+        "data_symbols_count": 163,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png",
+        "metric_id": [
+            "AUCTION_COLLAR_PRICE",
+            "AUCTION_HIGHEST_BID",
+            "AUCTION_LOWEST_ASK",
+            "AUCTION_PRICE",
+            "AUCTION_QUANTITY",
+            "AUCTION_RESULT",
+            "SYMBOL_DETAILS_MIN_ORDER_SIZE",
+            "SYMBOL_DETAILS_QUOTE_INCREMENT",
+            "SYMBOL_DETAILS_STATUS",
+            "SYMBOL_DETAILS_TICK_SIZE",
+            "SYMBOL_DETAILS_WRAP_ENABLED"
+        ]
+    },
+    {
+        "exchange_id": "ECB",
+        "website": "https://www.ecb.europa.eu/",
+        "name": "European Central Bank",
+        "data_quote_start": "2020-08-31T00:00:00.0000000Z",
+        "data_quote_end": "2023-08-29T00:00:00.0000000Z",
+        "data_trade_start": "2022-06-06T14:00:00.0000000Z",
+        "data_trade_end": "2022-06-10T14:00:00.0000000Z",
+        "data_symbols_count": 32,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0,
+
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+        
+    },
+    {
+        "exchange_id": "LMAXDIGITAL",
+        "website": "https://www.lmaxdigital.com/",
+        "name": "LMAX Digital",
+        "data_quote_start": "2020-12-03T00:00:00.0000000Z",
+        "data_quote_end": "2022-10-09T00:00:00.0000000Z",
+        "data_orderbook_start": "2022-01-02T00:00:00.0000000Z",
+        "data_orderbook_end": "2022-10-10T00:00:00.0000000Z",
+        "data_trade_start": "2020-12-03T00:00:00.0000000Z",
+        "data_trade_end": "2022-10-09T00:00:00.0000000Z",
+        "data_symbols_count": 16,
+        "volume_1hrs_usd": 0,
+        "volume_1day_usd": 0,
+        "volume_1mth_usd": 0
+        ,
+        "url": "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/20ddad0b4e4d4ea4ac92237292aa849b.png"
+    },
+]
 
 const ExchangeList = () => {
     return (
-<></>
+        <section>
+            <div className="flex flex-col mt-[10px]">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="px-20 py-3 text-left text-md font-semibold text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Exchanges
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-center text-md font-semibold text-gray-500 uppercase tracking-wider"
+                                        >
+                                            24H Trade Volume
+                                        </th>
+
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-center text-md font-semibold text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Remove the exchange
+                                        </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {exChangeList.map(listData => (
+                                        <tr key={listData.exchange_id}>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="flex items-center">
+                                                    <div className="flex-shrink-0 h-10 w-10">
+                                                        <img className="h-10 w-10 rounded-full" src={listData.url} alt="" />
+                                                    </div>
+                                                    <div className="ml-4">
+                                                        <div className="text-lg font-medium text-gray-900">{listData.name}</div>
+                                                        <a href={listData.website} className="text-md mt-2 text-gray-500">{listData.website}</a>
+
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 text-center whitespace-nowrap">
+                                                <div className="text-md font-bold text-gray-900">$ {listData.data_symbols_count} Billion</div>
+                                            </td>
+                                            <td scope="col"
+                                                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <a href="#" className="text-indigo-600 text-lg font-bold hover:text-indigo-900">
+                                                    Delete
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
 export default ExchangeList
+
+
+
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "tw-elements-react/dist/css/tw-elements-react.min.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
